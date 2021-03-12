31c0d781637539a8332b8d9f727e67619b3e54d3
Anand MS <49461181+ms-anand@users.noreply.github.com>
2021-03-12 12:28:32 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "terraform_rg" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "tf_vnet" {
+  name = "Terraform-VNet"
+  address_space = var.vnet_cidr
+  location = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+
+}
+
+resource "azurerm_subnet" "tf_subnet_1" {
+  name = "Subnet-1"
+  address_prefixes = var.subnet1_cidr
+  virtual_network_name = azurerm_virtual_network.tf_vnet.name
+  resource_group_name = azurerm_resource_group.terraform_rg.name
+}
+
+resource "azurerm_subnet" "tf_subnet_2" {
+  name = "Subnet-2"
+  address_prefixes = var.subnet2_cidr
+  virtual_network_name = azurerm_virtual_network.tf_vnet.name
+  resource_group_name = azurerm_resource_group.terraform_rg.name
+}
